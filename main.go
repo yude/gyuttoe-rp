c6035e9c23a5ddfd3dee7c5734a0f9e8fcfbb6eb
yudete <yu@yude.moe>
2020-10-26 18:45:03 +0900

    Setup window

--- main.go
+++ main.go
@@ -1,49 +1,107 @@
 package main
 
 import (
+	// base
 	"fmt"
 	"os"
 	"time"
 
+	// Discord Rich Presence Library
 	"github.com/hugolgst/rich-go/client"
+	// Load .env file
 	"github.com/joho/godotenv"
+	// GUI library "Fyne"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
-	// .env ファイルを読み込む (Discord の App ID 用)
+	// Load .env file (Discord Application ID)
 	err := godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
 	if err != nil {
 		panic(err)
 	}
 
-	// 教材の種類, 初期化
-	lesson := "None"
-	// HIKAKIN & SEIKIN - 今
+	// Define required variables
 	now := time.Now()
-	// Discord の App ID を .env から読み込む
-	// DiscordAppID := os.Getenv("DISCORD_APP_ID")
+	DiscordAppID := os.Getenv("DISCORD_APP_ID")
 
-	// Discord にログインする
-	errLogin := client.Login("770195570173804575")
+	// Login to Discord
+	errLogin := client.Login(DiscordAppID)
 	if errLogin != nil {
 		panic(errLogin)
 	}
 
-	// Rich Presence を表示する
-	errRP := client.SetActivity(client.Activity{
-		State:      "Playing",
-		Details:    lesson,
-		LargeImage: "e",
-		LargeText:  "ぎゅっとe - eラーニングシステム",
-		Timestamps: &client.Timestamps{
-			Start: &now,
-		},
-	})
+	// Fyne window setup
+	a := app.New()
+	w := a.NewWindow("ぎゅっとe Discord RPC") // Window title
 
-	if errRP != nil {
-		panic(errRP)
-	}
+	// Body of window
+	w.SetContent(widget.NewVBox(
+		widget.NewButton("Reading", func() { // Reading
+			client.SetActivity(client.Activity{
+				State:      "Playing",
+				Details:    "Reading",
+				LargeImage: "e",
+				LargeText:  "ぎゅっとe: eラーニングシステム",
+				Timestamps: &client.Timestamps{
+					Start: &now,
+				},
+			})
+		}),
+		widget.NewButton("Vocabulary", func() { // Vocabulary
+			client.SetActivity(client.Activity{
+				State:      "Playing",
+				Details:    "Vocabulary",
+				LargeImage: "e",
+				LargeText:  "ぎゅっとe: eラーニングシステム",
+				Timestamps: &client.Timestamps{
+					Start: &now,
+				},
+			})
+		}),
+		widget.NewButton("Listening", func() { // Listening
+			client.SetActivity(client.Activity{
+				State:      "Playing",
+				Details:    "Listening",
+				LargeImage: "e",
+				LargeText:  "ぎゅっとe: eラーニングシステム",
+				Timestamps: &client.Timestamps{
+					Start: &now,
+				},
+			})
+		}),
+		widget.NewButton("Grammar", func() { // Grammar
+			client.SetActivity(client.Activity{
+				State:      "Playing",
+				Details:    "Grammar",
+				LargeImage: "e",
+				LargeText:  "ぎゅっとe: eラーニングシステム",
+				Timestamps: &client.Timestamps{
+					Start: &now,
+				},
+			})
+		}),
+		widget.NewButton("MaG", func() { // ぎゅっとMaG
+			client.SetActivity(client.Activity{
+				State:      "Playing",
+				Details:    "ぎゅっとMaG",
+				LargeImage: "e",
+				LargeText:  "ぎゅっとe: eラーニングシステム",
+				Timestamps: &client.Timestamps{
+					Start: &now,
+				},
+			})
+		}),
+	))
 
-	fmt.Println("Sleeping...")
-	time.Sleep(time.Second * 10)
+	// Resize & Disable maximum button
+	w.Resize(fyne.NewSize(300, 150))
+	w.FixedSize()
+	w.SetFixedSize(true)
+
+	// Show window
+	w.ShowAndRun()
 }
